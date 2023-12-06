6072806a8231ebbdb7baff99a80ad6f7fc3bd54d
Bakeloh <bakelohnoel@gmail.com>
2023-12-06 16:41:51 +0300

    deleating already enlisted bot from list

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,6 +1,54 @@
-import React from "react";
+// import React from "react";
+
+// function BotCollection({ bots, enlistBot }) {
+//   return (
+//     <div className="ui four column grid">
+//       <div className="row">
+//         {bots.map((bot) => (
+//           <div key={bot.id} className="column">
+//             <div className="ui card">
+//               <div className="content">
+//                 <img src={bot.avatar_url} alt={bot.name} className="ui image" />
+//                 <div className="header">{bot.name}</div>
+//                 <div className="meta">{bot.bot_class}</div>
+//                 <div className="description">
+//                   <p>Health: {bot.health}</p>
+//                   <p>Damage: {bot.damage}</p>
+//                   <p>Armor: {bot.armor}</p>
+//                 </div>
+//                 <div className="extra content">
+//                   <button
+//                     className="ui primary button"
+//                     onClick={() => enlistBot(bot)}
+//                   >
+//                     Enlist
+//                   </button>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default BotCollection;
+
+// BotCollection.js
+import React, { useState } from "react";
 
 function BotCollection({ bots, enlistBot }) {
+  const [enlistedBots, setEnlistedBots] = useState([]);
+
+  const handleEnlist = (bot) => {
+    // Add the bot to the enlisted bots
+    setEnlistedBots([...enlistedBots, bot]);
+
+    // Call the parent component's enlistBot function
+    enlistBot(bot);
+  };
+
   return (
     <div className="ui four column grid">
       <div className="row">
@@ -19,7 +67,7 @@ function BotCollection({ bots, enlistBot }) {
                 <div className="extra content">
                   <button
                     className="ui primary button"
-                    onClick={() => enlistBot(bot)}
+                    onClick={() => handleEnlist(bot)}
                   >
                     Enlist
                   </button>
--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -1,114 +1,13 @@
-// import React, { useEffect, useState } from "react";
-// import YourBotArmy from "./YourBotArmy";
-// import BotCollection from "./BotCollection";
-
-// function BotsPage() {
-//   const [bots, setBots] = useState([]);
-
-//   useEffect(() => {
-//     const fetchBots = async () => {
-//       try {
-
-//         const response = await fetch("http://localhost:8002/bots");
-//         const data = await response.json();
-//         setBots(data);
-//       } catch (error) {
-//         console.error("Error fetching bots:", error);
-//       }
-//     };
-
-//     fetchBots();
-//   }, []);
-
-//   return (
-//     <div>
-//       <YourBotArmy bots={bots} />
-//       <BotCollection bots={bots} />
-//       {/* <YourBotArmy enlistedBots={selectedBots} dischargeBot={dischargeBot} />
-//       <BotCollection allBots={allBots} enlistBot={enlistBot} releaseBot={releaseBot} /> */}
-//     </div>
-//   );
-// }
-
-// export default BotsPage;
-
-
-
-
-
-// BotPage.js
-// import React, { useState, useEffect } from "react";
-// import YourBotArmy from "./YourBotArmy";
-// import BotCollection from "./BotCollection";
-
-// function BotPage() {
-//   const [allBots, setAllBots] = useState([]);
-//   const [selectedBots, setSelectedBots] = useState([]);
-
-//   useEffect(() => {
-//     // Fetch all bots from your API and update the state
-//     const fetchData = async () => {
-//       try {
-//         const response = await fetch("http://localhost:8002/bots");
-//         const data = await response.json();
-//         setAllBots(data);
-//       } catch (error) {
-//         console.error("Error fetching bots:", error);
-//       }
-//     };
-
-//     fetchData();
-//   }, []);
-
-//   const enlistBot = (bot) => {
-//     // Check if the bot is not already enlisted
-//     if (!selectedBots.find((selectedBot) => selectedBot.id === bot.id)) {
-//       setSelectedBots([...selectedBots, bot]);
-//     }
-//   };
-
-//   const releaseBot = (bot) => {
-//     // Remove the bot from the enlisted bots
-//     const updatedBots = selectedBots.filter((selectedBot) => selectedBot.id !== bot.id);
-//     setSelectedBots(updatedBots);
-//   };
-
-//   const dischargeBot = async (bot) => {
-//     // Remove the bot from both frontend and backend
-//     try {
-//       // Delete the bot on the backend
-//       await fetch(`http://localhost:8002/bots/${bot.id}`, {
-//         method: "DELETE",
-//       });
-
-//       // Remove the bot from the enlisted bots on the frontend
-//       const updatedBots = selectedBots.filter((selectedBot) => selectedBot.id !== bot.id);
-//       setSelectedBots(updatedBots);
-//     } catch (error) {
-//       console.error("Error discharging bot:", error);
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <YourBotArmy enlistedBots={selectedBots} dischargeBot={dischargeBot} />
-//       <BotCollection allBots={allBots} enlistBot={enlistBot} releaseBot={releaseBot} />
-//     </div>
-//   );
-// }
-
-// export default BotPage;
-
 import React, { useState, useEffect } from "react";
 import YourBotArmy from "./YourBotArmy";
 import BotCollection from "./BotCollection";
 
 function BotPage() {
   const [allBots, setAllBots] = useState([]);
-  const [selectedBots, setSelectedBots] = useState([]);
+  const [enlistedBots, setEnlistedBots] = useState([]);
 
   useEffect(() => {
-    // Fetch all bots from your API and update the state
+
     const fetchData = async () => {
       try {
         const response = await fetch("http://localhost:8002/bots");
@@ -123,23 +22,23 @@ function BotPage() {
   }, []);
 
   const enlistBot = (bot) => {
-    // Check if the bot is not already enlisted
-    if (!selectedBots.find((selectedBot) => selectedBot.id === bot.id)) {
-      setSelectedBots([...selectedBots, bot]);
+
+    if (!enlistedBots.find((enlistedBot) => enlistedBot.id === bot.id)) {
+      setEnlistedBots([...enlistedBots, bot]);
     }
   };
 
   const dischargeBot = async (bot) => {
-    // Remove the bot from both frontend and backend
+
     try {
-      // Delete the bot on the backend
+
       await fetch(`http://localhost:8002/bots/${bot.id}`, {
         method: "DELETE",
       });
 
-      // Remove the bot from the enlisted bots on the frontend
-      const updatedBots = selectedBots.filter((selectedBot) => selectedBot.id !== bot.id);
-      setSelectedBots(updatedBots);
+
+      const updatedEnlistedBots = enlistedBots.filter((enlistedBot) => enlistedBot.id !== bot.id);
+      setEnlistedBots(updatedEnlistedBots);
     } catch (error) {
       console.error("Error discharging bot:", error);
     }
@@ -147,11 +46,10 @@ function BotPage() {
 
   return (
     <div>
-      <YourBotArmy enlistedBots={selectedBots} dischargeBot={dischargeBot} />
+      <YourBotArmy enlistedBots={enlistedBots} dischargeBot={dischargeBot} />
       <BotCollection bots={allBots} enlistBot={enlistBot} />
     </div>
   );
 }
 
 export default BotPage;
-
