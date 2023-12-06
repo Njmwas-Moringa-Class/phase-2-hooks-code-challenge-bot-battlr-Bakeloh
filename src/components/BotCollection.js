90f3c5ff024f7f8b19a166363e79e393e92eb736
Bakeloh <bakelohnoel@gmail.com>
2023-12-06 16:29:11 +0300

    Completed Assignment

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,12 +1,33 @@
 import React from "react";
 
-function BotCollection() {
-  // Your code here
+function BotCollection({ bots, enlistBot }) {
   return (
     <div className="ui four column grid">
       <div className="row">
-        {/*...and here..*/}
-        Collection of all bots
+        {bots.map((bot) => (
+          <div key={bot.id} className="column">
+            <div className="ui card">
+              <div className="content">
+                <img src={bot.avatar_url} alt={bot.name} className="ui image" />
+                <div className="header">{bot.name}</div>
+                <div className="meta">{bot.bot_class}</div>
+                <div className="description">
+                  <p>Health: {bot.health}</p>
+                  <p>Damage: {bot.damage}</p>
+                  <p>Armor: {bot.armor}</p>
+                </div>
+                <div className="extra content">
+                  <button
+                    className="ui primary button"
+                    onClick={() => enlistBot(bot)}
+                  >
+                    Enlist
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,14 +1,71 @@
+// import React from "react";
+
+// function YourBotArmy({ bots }) {
+//   return (
+//     <div className="ui segment inverted olive bot-army">
+//       <div className="ui five column grid">
+//         <div className="row bot-army-row">
+//           {bots.map((bot) => (
+//             <div key={bot.id} className="column">
+//               <div className="ui card">
+//                 <div className="content">
+//                   <div className="header">{bot.name}</div>
+//                   <div className="meta">{bot.bot_class}</div>
+//                   <div className="description">
+//                     <p>Health: {bot.health}</p>
+//                     <p>Damage: {bot.damage}</p>
+//                     <p>Armor: {bot.armor}</p>
+//                     {/* Add more details as needed */}
+//                   </div>
+//                   {/* Add more actions or details if needed */}
+//                 </div>
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default YourBotArmy;
+
+
+// YourBotArmy.js
+// YourBotArmy.js
+// YourBotArmy.js
+// YourBotArmy.js
 import React from "react";
 
-function YourBotArmy() {
-  //your bot army code here...
-
+function YourBotArmy({ enlistedBots, dischargeBot }) {
   return (
     <div className="ui segment inverted olive bot-army">
       <div className="ui five column grid">
         <div className="row bot-army-row">
-          {/*...and here...*/}
-          Your Bot Army
+          {enlistedBots.map((bot) => (
+            <div key={bot.id} className="column">
+              <div className="ui card">
+                <div className="content">
+                  <img src={bot.avatar_url} alt={bot.name} className="ui image" />
+                  <div className="header">{bot.name}</div>
+                  <div className="meta">{bot.bot_class}</div>
+                  <div className="description">
+                    <p>Health: {bot.health}</p>
+                    <p>Damage: {bot.damage}</p>
+                    <p>Armor: {bot.armor}</p>
+                  </div>
+                  <div className="extra content">
+                    <button
+                      className="ui red button"
+                      onClick={() => dischargeBot(bot)}
+                    >
+                      Discharge
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
@@ -16,3 +73,7 @@ function YourBotArmy() {
 }
 
 export default YourBotArmy;
+
+
+
+
